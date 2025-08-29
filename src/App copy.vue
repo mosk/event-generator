06d21e60e4cea3b378359aad9d26ae4f1b461124
Mihail <miiixsc@gmail.com>
2025-08-29 07:20:08 +0300

    *

--- src/App copy.vue
+++ src/App copy.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
-import PageDescription from './components/PageDescription.vue'
-import EventForm from './components/EventForm.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <PageDescription />
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <EventForm />
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
   </main>
 </template>
 
---
+++ src/components/EventForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <form action="">
+    <p>Событие</p>
+
+    <div class="dates">
+      <label for="date-start">Начало</label>
+      <input
+        id="date-start"
+        name="date-start"
+        type="datetime-local"
+        value="2018-06-12T19:30"
+        min="2018-06-07T00:00"
+      />
+
+      <label for="date-end">Окончание</label>
+      <input
+        id="date-end"
+        name="date-end"
+        type="datetime-local"
+        value="2018-06-12T19:30"
+        min="2018-06-07T00:00"
+      />
+    </div>
+
+    <label for="timezone">Часовой пояс</label>
+    <select name="timezone" id="timezone">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>
+  </form>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
